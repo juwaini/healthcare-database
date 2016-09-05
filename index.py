5496e7175ce074258ffbe4474a3cda54608c5ef9
juwaini <juwaini@gmail.com>
2016-09-05 22:09:29 +0800

    Add bootstrap to static/ directory

--- index.py
+++ index.py
@@ -1,11 +1,11 @@
 from flask import Flask 
 from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/<name>')
-def hello(name=None):
-    return render_template('index.html', name=name)
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
--- tests/functional_test.py
+++ tests/functional_test.py
@@ -9,11 +9,6 @@ class PageFunctionalCase(unittest.TestCase):
     def test_index_sanity_test(self):
         self.browser.get('localhost:5000')
         self.assertIn('Welcome To Healthcare Database System', self.browser.title)
-        #self.assertIn('Hello world!', self.browser.page_source)
-
-    def test_page_with_name(self):
-        self.browser.get('localhost:5000/juwaini')
-        self.assertIn('Hello juwaini!', self.browser.page_source)
 
     def tearDown(self):
         self.browser.quit()
