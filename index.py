33321a43001e4ee09212c147361debc2cacd1935
juwaini <juwaini@gmail.com>
2016-09-05 22:28:56 +0800

    Add REST API boilerplate

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 from flask import Flask 
+from flask import jsonify 
 from flask import render_template
 
 app = Flask(__name__)
@@ -6,6 +7,16 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/api/patients/', methods=["GET"])
+def patients():
+    value = {'name': 'Ahmad Albab.'}
+    return jsonify(value)
+
+@app.route('/api/hospitals/', methods=["GET"])
+def hospitals():
+    value = {'name': 'Hospital Shah Alam'}
+    return jsonify(value)
 
 if __name__ == '__main__':
     app.run()
